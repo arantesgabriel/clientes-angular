d61515152a0c7e0cc7b1488006e0cdd52b668508
Gabriel Arantes <gabrielpacontato@gmail.com>
2023-06-12 15:48:30 -0300

    Criando repositório no GitHub.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'clientes-angular';
+  title = 'Bem vindo ao Sistema de Clientes!';
 }
