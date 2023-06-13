00d7176030b7066177e0420de3727c2839fa6ffd
Gabriel Arantes <gabrielpacontato@gmail.com>
2023-06-13 17:15:45 -0300

    Iniciado o processo de criação de funções para validação de campos.

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -3,9 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
   nome: string = '';
   senha: string = '';
+
+  cadastrar(): void {
+    let inputUsuario = document.getElementById('input-usuario');
+    let inputSenha = document.getElementById('input-senha');
+    this.validar();
+  }
+
+  validar(): void {
+    let inputUsuario = document.getElementById('input-usuario');
+    let inputSenha = document.getElementById('input-senha');
+
+    if (inputUsuario?.textContent === "") {
+      alert("O campo usuário está vazio")
+    }
+
+}
 }
