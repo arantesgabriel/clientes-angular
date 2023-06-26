b6f222def36ae02511a9650cfa274d294e695055
Gabriel Arantes <gabrielpacontato@gmail.com>
2023-06-26 17:07:04 -0300

    Adicionados a tela de cadastro o título, campos de input e o botão de criação de conta com animação.

--- src/app/views/cadastro/cadastro.component.ts
+++ src/app/views/cadastro/cadastro.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.scss']
+  styleUrls: ['./cadastro.component.scss'],
 })
 export class CadastroComponent {
+  senhaCadastro!: string;
+  loading: boolean = false;
+
+  cadastroForm = new FormGroup({
+    userInput: new FormControl(''),
+    emailInput: new FormControl(''),
+    passwordInput: new FormControl('')
+  });
+
+  // Método que realiza o efeito de loading do botão criar conta
+  load() {
+      this.loading = true;
+
+      setTimeout(() => {
+          this.loading = false
+      }, 2000);
+  }
 
 }
